55d0e5db88f7669ebe95879c8013b99a3f72d461
noogler-eng <sharadpoddar10102003@gmail.com>
2024-12-10 22:01:27 +0530

    add configuration

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const JSON_RPC_ENDPOINT = process.env.JSON_RPC_ENDPOINT || "";
+const API_KEY = process.env.API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    amoy_polygon: {
+      url: JSON_RPC_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
+  etherscan: {
+    apiKey: API_KEY,
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
 
 export default config;
