5cd97da311cc077c06bd2db654b0f22af91d3eb5
noogler-eng <sharadpoddar10102003@gmail.com>
2024-12-10 21:55:13 +0530

    first commit

---
+++ hardhat/script/deploy.ts
@@ -0,0 +1,20 @@
+import hardhat from "hardhat";
+
+const deploy_contract = async (): Promise<string> => {
+  try {
+    const my_contract = await hardhat.ethers.getContractFactory("contract");
+    const contract = await my_contract.deploy();
+    return contract.target.toString();
+  } catch (error) {
+    console.log(error);
+    return "contract has not been deployed";
+  }
+};
+
+deploy_contract()
+  .then((address: string) => {
+    console.log("deplyed contract address: ", address);
+  })
+  .catch((error: Error) => {
+    console.log("error while deploying contract: ", error);
+  });
