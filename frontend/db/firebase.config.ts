e7d84eee45f65d1719156e35b227a25eb975ac2f
noogler-eng <sharadpoddar10102003@gmail.com>
2024-12-24 16:33:17 +0530

    fixing

--- frontend/db/firebase.config.ts
+++ frontend/db/firebase.config.ts
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import dotenv from "dotenv";
+dotenv.config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.VITE_API_KEY,
-  authDomain: process.env.VITE_AUTH_DOMAIN,
-  projectId: process.env.VITE_PROJECT_ID,
-  storageBucket: process.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_MSG_SENDER_ID,
-  appId: process.env.VITE_APP_ID,
-  measurementId: process.env.VITE_MES_ID,
+  apiKey: process.env.VITE_API_KEY || "",
+  authDomain: process.env.VITE_AUTH_DOMAIN || "",
+  projectId: process.env.VITE_PROJECT_ID || "",
+  storageBucket: process.env.VITE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.VITE_MSG_SENDER_ID || "",
+  appId: process.env.VITE_APP_ID || "",
+  measurementId: process.env.VITE_MES_ID || "",
 };
 
 // Initialize Firebase
